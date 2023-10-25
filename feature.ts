f50708904ad045a814195402e9c2cc10333e52aa
alaa-abuhani <alaa.abuhani@gmail.com>
2023-10-25 20:26:17 +0300

    resolve coflict

--- feature.ts
+++ feature.ts
@@ -1,10 +1,4 @@
 // Calculate the sum of two numbers
 function calculateSum(a: number, b: number): number {
-    return a + b;
+  return a + b;
 }
-<<<<<<< HEAD
-//console.log(calculateSum);
-=======
-// console.log
-// change 2
->>>>>>> feature-branch
